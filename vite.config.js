014ccfb83b0e162c8498217f4b0d1b60d072e89c
alfianchii <pianaee379@gmail.com>
2024-03-30 20:34:01 +0700

    chore: update Vite configs

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,43 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import fs from "fs";
+import path, { resolve } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const root = resolve(__dirname, "resources");
+
+const resourceFiles = (dirPath, filesArray = []) => {
+    const paths = fs.readdirSync(dirPath);
+
+    for (const path of paths) {
+        // If .blade.php file, skip it
+        if (!path.includes(".blade.php")) {
+            const basePath = resolve(dirPath, path);
+            const pathStat = fs.statSync(basePath);
+            pathStat.isDirectory()
+                ? resourceFiles(basePath, filesArray)
+                : filesArray.push(basePath);
+        }
+    }
+
+    return filesArray;
+};
+const files = resourceFiles(root);
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+    plugins: [laravel({ input: files })],
+    server: {
+        host: "0.0.0.0",
+        hmr: {
+            clientPort: 3000,
+            host: "127.0.0.1",
+        },
+        port: 3000,
+        open: false,
+        watch: {
+            usePolling: true,
+        },
+    },
 });
